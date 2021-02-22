cdd955e7f13d796894a85dbd9c7ed44ce59e99a1
*esomagsorc894* <you@example.com>
2021-02-22 15:05:22 -0500

    meta tag

--- utility.js
+++ utility.js
@@ -1,4 +1,4 @@
 
-  // document.addEventListener('touchmove', function (event) {
-  //   if (event.scale !== 1) { event.preventDefault(); }
-  // }, { passive: false });
+  document.addEventListener('touchmove', function (event) {
+    if (event.scale !== 1) { event.preventDefault(); }
+  }, { passive: false });
